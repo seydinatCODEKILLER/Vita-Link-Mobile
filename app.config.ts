e4eb8adfa240188a69593c3e66beb44c91db875c
Seydina Thiam <seydinat235@gmail.com>
2026-05-14 13:26:39 +0000

    feat: ajouter la configuration de l'application et remplacer app.json par app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,62 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Vita-Link-Mobile",
+  slug: "Vita-Link-Mobile",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "vitalinkmobile",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "sn.vitalink.mobile",
+  },
+  android: {
+    package: "sn.vitalink.mobile",
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+    bundler: "metro",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: {
+          backgroundColor: "#000000",
+        },
+      },
+    ],
+    "expo-secure-store",
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+
+  extra: {
+    apiUrl: process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000/api",
+    socketUrl: process.env.EXPO_PUBLIC_SOCKET_URL || "http://localhost:3000",
+    eas: {
+      projectId: "d7a0faeb-fe87-4418-b24c-394df580ca71",
+    },
+  },
+});
